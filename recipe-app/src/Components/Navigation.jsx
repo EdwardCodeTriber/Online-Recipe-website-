16ba78d78d99d79bc211eb7ee782ea8d533154a6
EdwardThapelo <edwardthapelo55@gmail.com>
2024-08-12 16:46:01 +0200

    Moved Coponents

--- recipe-app/src/Components/Navigation.jsx
+++ recipe-app/src/Components/Navigation.jsx
@@ -21,13 +21,7 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import TextField from '@mui/material/TextField';
 
-import AddIcon from "@mui/icons-material/Add";
-import Paper from "@mui/material/Paper";
 
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 
 const drawerWidth = 240;
 
@@ -51,11 +45,7 @@ function Navigation(props) {
     }
   };
 
-  const [category, setCategory] = React.useState('');
-
-  const handleChange = (event) => {
-    setCategory(event.target.value);
-  };
+  
 
   const drawer = (
     <div>
@@ -208,86 +198,7 @@ function Navigation(props) {
         <Toolbar />
         <Typography paragraph>
           {/* Show Content in a paper component */}
-          <form >
-        <Paper
-          elevation={5}
-          component="paper"
-          sx={{
-            p: "2px 4px",
-            display: "flex",
-            flexDirection:"column",
-            alignItems: "center",
-            width: 400,
-            height: 450,
-            gap: 1,
-          }}
-        >
-          <TextField
-          sx={{
-            margin:"auto"
-          }}
-          required
-          id="filled-required"
-          label="Recipe"
-        //   defaultValue="Hello World"
-          variant="filled"
-        />
           
-          <TextField
-          sx={{
-            margin:"auto"
-          }}
-          id="outlined-multiline-static"
-          label="Ingredients"
-          multiline
-          rows={1}
-        //   defaultValue="Default Value"
-        />
-        <TextField
-        sx={{
-            margin:"auto"
-          }}
-          id="outlined-multiline-static"
-          label="Instructions"
-          multiline
-          rows={1}
-        //   defaultValue="Default Value"
-        />
-        <InputLabel id="demo-simple-select-label"></InputLabel>
-        <Select
-          sx={{
-            margin:"auto",
-            width:0.5,
-          }}
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={category}
-          label="category"
-          onChange={handleChange}
-        >
-          <MenuItem value={10}>Dessert</MenuItem>
-          <MenuItem value={20}>Main Course</MenuItem>
-          <MenuItem value={30}>Appetiser</MenuItem>
-        </Select>
-        <TextField
-          id="filled-number"
-          label="Cooking-Time in minutes"
-          type="number"
-          onChange={(e) => setTask({ ...tasks, task: e.target.value })}
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="filled"
-        />
-          
-          
-          {/* <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" /> */}
-          {/* <button type="submit"> add</button> */}
-          <IconButton type="submit" sx={{ p: "10px" }} aria-label="add">
-            <AddIcon/>
-          </IconButton>
-        </Paper>
-      </form>
         </Typography>
         <Typography paragraph>
           {/* Show Content in a paper component */}
--- recipe-app/src/Pages/Popadd.jsx
+++ recipe-app/src/Pages/Popadd.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import Paper from "@mui/material/Paper";
-import InputBase from "@mui/material/InputBase";
-import { green, yellow, red } from "@mui/material/colors";
-import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import AddIcon from "@mui/icons-material/Add";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+
 import axios from "axios";
 
 const Popadd = () => {
@@ -19,6 +20,11 @@ const Popadd = () => {
     Cookingtime
 
   });
+  const [category, setCategory] = React.useState('');
+
+  const handleChange = (event) => {
+    setCategory(event.target.value);
+  };
 
   const handleForm = (e) => {
     e.preventDefault();
@@ -45,29 +51,73 @@ const Popadd = () => {
           sx={{
             p: "2px 4px",
             display: "flex",
+            flexDirection:"column",
             alignItems: "center",
             width: 400,
+            height: 450,
+            gap: 1,
           }}
         >
-          <InputBase
-            sx={{ ml: 1, flex: 1, border: "none" }}
-            placeholder="Recipe Name"
-            name="name"
-            type="text"
-            onChange={(e) => setTask({ ...tasks, task: e.target.value })}
-            inputProps={{ "aria-label": "Recipe" }}
-          />
-          <InputBase
-            sx={{ ml: 1, flex: 1, border: "none" }}
-            placeholder="Ingredients"
-            name="ingrediens"
-            type="text"
-            onChange={(e) => setTask({ ...tasks, task: e.target.value })}
-            inputProps={{ "aria-label": "ingrediens" }}
-          />
+          <TextField
+          sx={{
+            margin:"auto"
+          }}
+          required
+          id="filled-required"
+          label="Recipe"
+        //   defaultValue="Hello World"
+          variant="filled"
+        />
+          
+          <TextField
+          sx={{
+            margin:"auto"
+          }}
+          id="outlined-multiline-static"
+          label="Ingredients"
+          multiline
+          rows={1}
+        //   defaultValue="Default Value"
+        />
+        <TextField
+        sx={{
+            margin:"auto"
+          }}
+          id="outlined-multiline-static"
+          label="Instructions"
+          multiline
+          rows={1}
+        //   defaultValue="Default Value"
+        />
+        <InputLabel id="demo-simple-select-label">Category</InputLabel>
+        <Select
+          sx={{
+            margin:"auto",
+            width:0.5,
+          }}
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={category}
+          label="category"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Dessert</MenuItem>
+          <MenuItem value={20}>Main Course</MenuItem>
+          <MenuItem value={30}>Appetiser</MenuItem>
+        </Select>
+        <TextField
+          id="filled-number"
+          label="Cooking-Time in minutes"
+          type="number"
+          onChange={(e) => setTask({ ...tasks, task: e.target.value })}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="filled"
+        />
           
           
-          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+          {/* <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" /> */}
           {/* <button type="submit"> add</button> */}
           <IconButton type="submit" sx={{ p: "10px" }} aria-label="add">
             <AddIcon/>
