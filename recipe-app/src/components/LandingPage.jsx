4fe6d61602a96ac5afe29e9e0278004ab37b992f
EdwardThapelo <edwardthapelo55@gmail.com>
2024-10-02 13:21:58 +0200

    Modified the landing component

--- recipe-app/src/components/LandingPage.jsx
+++ recipe-app/src/components/LandingPage.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import { Container, Typography, Button, Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import picture from '../assets/back.jpg'; 
 
 const LandingPage = () => {
   const navigate = useNavigate();
 
   return (
-    <Container>
+    <Container
+      maxWidth="xm"
+      style={{
+        backgroundImage: `url(${picture})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        padding: '20px',
+        borderRadius: '8px',
+        minHeight: '100vh',  
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden', 
+      }}
+    >
       <Box
         display="flex"
         flexDirection="column"
         justifyContent="center"
         alignItems="center"
-        minHeight="100vh"
+        bgcolor="rgba(255, 255, 255, 0.8)"  
+        p={4} 
+        borderRadius={2}  
         textAlign="center"
+        width="100%"  
       >
         <Typography variant="h2" gutterBottom>
           Welcome to Recipe Manager
